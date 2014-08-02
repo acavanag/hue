af9f8addcfea995e5e2adaae9c79d1a78df32b86
acavanaghww <acavanagh@weddingwire.com>
2014-08-02 17:27:43 -0400

    Initial Commit

---
+++ OBJCHue/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OBJCHue
+//
+//  Created by Andrew Cavanagh on 8/2/14.
+//  Copyright (c) 2014 WeddingWire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
